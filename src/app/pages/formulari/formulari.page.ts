65ca8959c615f03e5b2a98acd556596845726931
JoaquimCF <quimcabello@gmail.com>
2021-12-04 00:06:25 +0100

    all big changes

---
+++ src/app/pages/formulari/formulari.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-formulari',
+  templateUrl: './formulari.page.html',
+  styleUrls: ['./formulari.page.scss'],
+})
+export class FormulariPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
